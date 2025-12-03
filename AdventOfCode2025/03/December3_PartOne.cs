71f591963995a1be8108a5bafa454b6b48819aaf
Olav Fykse <olav.fykse@arbeidstilsynet.no>
2025-12-03 12:37:25 +0100

    Dec 3 - part 1

---
+++ AdventOfCode2025/03/December3_PartOne.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode2025._03;
+
+public class December3_PartOne
+{
+    public static long CalculateSumJoltage()
+    {
+        var banks = File.ReadLines("../../../03/PuzzleInput.txt").ToList();
+        long sum = 0;
+        
+        foreach (var bank in banks)
+        {
+            long currentMax = 0;
+            for (int i = 0; i < bank.Length; i++)
+            {
+                var firstDigit = bank[i];
+                for (int j = i + 1; j < bank.Length; j++)
+                {
+                    var secondDigit = bank[j];
+                    var tempSum = long.Parse($"{firstDigit}{secondDigit}");
+                    if (tempSum > currentMax)
+                    {
+                        currentMax = tempSum;
+                    }
+                }
+            }
+            Console.WriteLine(currentMax);
+            sum += currentMax;
+        }
+        return sum;
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -2,6 +2,7 @@
 
 using AdventOfCode2025._01;
 using AdventOfCode2025._02;
+using AdventOfCode2025._03;
 
 // Day 1
 // Console.WriteLine($"December 1 - Part 1: {December1_PartOne.CalculatePassword()}");
@@ -9,5 +10,8 @@ using AdventOfCode2025._02;
 
 // Day 2
 // Console.WriteLine($"December 2 - Part 1: {December2_PartOne.CalculateSumOfInvalidIds()}");
-Console.WriteLine($"December 2 - Part 2: {December2_PartTwo.CalculateSumOfInvalidIds()}");
+// Console.WriteLine($"December 2 - Part 2: {December2_PartTwo.CalculateSumOfInvalidIds()}");
+
+// Day 3
+Console.WriteLine($"December 3 - Part 1: {December3_PartOne.CalculateSumJoltage()}");
 
